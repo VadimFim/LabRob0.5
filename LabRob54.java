01be000ebaa787bb09a56a0a26339b1652130ae6
VadimFim <sivanlt943@gmail.com>
2023-06-04 21:41:53 +0300

    Create LabRob54.java

---
+++ LabRob54.java
@@ -0,0 +1,7 @@
+public class LabRob54 {
+    public static void main(String[] arg){
+        System.out.println("Батько: Загоруйко Віталій 1979 року народження, водій швидкої;");
+        System.out.println("Мати: Загоруйко Софія, 1984 року народження, хімік;");
+        System.out.println("Сестра: Загоруйко Валерія, 2010 року народження, учениця;");
+    }
+}
